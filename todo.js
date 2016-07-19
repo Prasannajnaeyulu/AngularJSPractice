e189b70b5f16399f24e01e2499502e3d65ba96bf
Santosh Chaitanya <contactme.santhu@gmail.com>
2016-07-19 12:15:01 +0530

    AngularJSPractice set to get into action

--- todo.js
+++ todo.js
@@ -1,27 +1,37 @@
-angular.module("todoapp", []).
-controller("TodoListController", function(){
-   var todoList = this;
-   todoList.todos=[{text:"learn english" done:true},{text:"learn hadoop"   done: false}];
-   
-   todoList.addTodo = function(){
-       todoList.todos.push({text:todoList.todoText, done:false}});
-       todoList.todoText='';
-   };
+angular.module("todoapp", [])
+  .controller("TodoListController", ['$scope', function($scope) {
+    var todoList = {};
+    todoList.todos = [{
+        "text": "learn english",
+        "done": true
+    }, {
+        "text": "learn hadoop",
+        "done": true
+    }];
 
-   todoList.remaining = function(){
-       var count =0;
-       angular.forEach(todoList.todos, function(todo){
-           count+=todo.done?0:1;
-       });
-      return count;
-   };
+    todoList.addTodo = function() {
+        todoList.todos.push({
+            text: todoList.todoText,
+            done: false
+        });
+        todoList.todoText = '';
+    };
 
-   todoList.archive=function(){
-         var oldtodos = todoList.todos;
-         todoList.todos=[];
-         angular.forEach(oldtodos, function(todo){
-          if(!todo.done) todoList.todos.push(todo);
-	});
-  };
-          
- });
+    todoList.remaining = function() {
+        var count = 0;
+        angular.forEach(todoList.todos, function(todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
+    };
+
+    todoList.archive = function() {
+        var oldtodos = todoList.todos;
+        todoList.todos = [];
+        angular.forEach(oldtodos, function(todo) {
+            if (!todo.done) todoList.todos.push(todo);
+        });
+    };
+
+    $scope.todoList = todoList;
+ }]);
